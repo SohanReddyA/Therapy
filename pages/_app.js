2f1a79babdceeb9e98332ddf4d938b18fa1a2f2c
SohanReddyA <pingusohan@gmail.com>
2023-07-04 22:51:25 +0530

    Before font change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,22 @@
 import '@/styles/globals.css';
 import { Toaster } from 'react-hot-toast';
 import { ThemeProvider } from 'styled-components';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  weight: ['400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <>
+      {' '}
+      <style jsx global>{`
+        html {
+          font-family: ${poppins.style.fontFamily};
+        }
+      `}</style>
       <Toaster />
       <Component {...pageProps} />
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -34,16 +34,6 @@ export default class MyDocument extends Document {
       <Html>
         <Head>
           <title>Friennly</title>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            rel="preconnect"
-            href="https://fonts.gstatic.com"
-            crossOrigin=""
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;400;600;700&display=swap"
-            rel="stylesheet"
-          />
         </Head>
         <body>
           <Main />
