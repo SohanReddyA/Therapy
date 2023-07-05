bfc0088b6cdf12072ddaef51d80c36f79d8e22d4
Sudeep Kumar Nemani <sudeep459@gmail.com>
2023-07-05 11:30:38 +0530

    Add new chat components

---
+++ src/pages/chat/components/message-card/index.js
@@ -0,0 +1,1 @@
+export {default as MessageCard} from './template'
--- src/pages/chat/template.jsx
+++ src/pages/chat/template.jsx
@@ -1,17 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-import { AiOutlineSearch } from 'react-icons/ai';
-import { BsFilter } from 'react-icons/bs';
-import ChatCard from './components/chat-card/template';
+import React, { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import { AiOutlineSearch } from "react-icons/ai";
+import { BsFilter, BsEmojiSmile } from "react-icons/bs";
+import { ImAttachment } from "react-icons/im";
+import { IoMdSend } from "react-icons/io";
+import ChatCard from "./components/chat-card/template";
+import { MessageCard } from "./components/message-card";
 
 const Chat = () => {
   const router = useRouter();
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [currentChat, setCurrentChat] = useState(null);
+  const [content, setContent] = useState("");
+  const [paymentDone, setPaymentDone] = useState(false);
 
   const handleClickOnChatCard = () => {
     setCurrentChat(true);
   };
+
+  const handleCreateMessage = () => {};
 
   const handleSearch = (e) => {
     setQuery(e);
@@ -85,16 +92,17 @@ const Chat = () => {
 
   return (
     <div className="bg-[#E5E5E5] flex items-center justify-center h-screen">
-      <div className="flex items-center bg-white justify-center w-[95%] h-[95%] rounded-md">
+      <div className="flex items-center relative bg-white justify-center w-[95%] h-[95%] rounded-md">
         <div className="left w-[30%] border-[#D5C9EB] border-r-2 flex-col items-center justify-center">
           {/*Title of the page */}
           <div className="h-[10vh]">
             <h1
               className="font-bold h-full text-[#5627B0] px-3 py-10 mx-7 text-3xl cursor-pointer"
               onClick={() => {
-                router.push('/');
-              }}>
-              Friennly
+                router.push("/");
+              }}
+            >
+              Therap.ai
             </h1>
           </div>
           {/* Chat Message Section */}
@@ -117,9 +125,9 @@ const Chat = () => {
             </div>
 
             {/* Chat List */}
-            <div className="bg-white overflow-y-scroll scroll-smooth h-[75vh] px-3">
-              {[1, 1, 1, 1, 1].map((list, index) => (
-                <div onClick={handleClickOnChatCard} key={index}>
+            <div className="bg-white overflow-auto h-[75vh] px-3">
+              {[1, 1, 1, 1, 1, 1, 1].map((list) => (
+                <div onClick={handleClickOnChatCard}>
                   {query && <ChatCard />}
                 </div>
               ))}
@@ -127,29 +135,104 @@ const Chat = () => {
           </div>
         </div>
 
-        <div className="right w-[70%] flex-col items-center justify-center">
+        <div className="right w-[70%] flex-col relative items-center justify-center">
           {/* Default start page */}
           {!currentChat && (
             <div className="flex flex-col items-center text-center max-width-[70%]">
               <img className="w-52 h-52" src="/images/testLogo2.svg" alt="" />
               <p className="my-9 text-2xl font-medium text-[#5627B0]">
-                Chat with your favorite therapist now!{' '}
+                Chat with your favorite therapist now!{" "}
               </p>
             </div>
           )}
           {/* Message Part */}
           {currentChat && (
             <div>
-              <div className="h-[10vh] border-b-2 border-[#D5C9EB]">
-                <img
-                  src="/images/testLogo2.svg"
-                  className="w-14 h-14 rounded-full"
-                  alt=""
-                />
-                <p>Jessica Drew</p>
+              {/* Header */}
+              <div className="h-[10vh] border-b-2 border-[#D5C9EB] flex items-center justify-between">
+                <div className="flex items-center p-3 space-x-4">
+                  <img
+                    src="/images/testLogo2.svg"
+                    className="w-14 h-14 rounded-full"
+                    alt=""
+                  />
+                  <div className="flex flex-col items-start justify-center">
+                    <p className="font-semibold text-lg">Jessica Drew</p>
+                    <p className="text-[#5F5F5F] text-sm">
+                      last seen 5 min ago
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <div className="p-3 rounded-3xl mx-10 bg-[#EEE9F7]">
+                    <p className="text-[#5627B0] font-semibold">
+                      Time left: 17hrs 30min
+                    </p>
+                  </div>
+                </div>
               </div>
+              {/* Message Section */}
+              <div className="px-10 h-[85vh] overflow-auto">
+                <div className="space-y-2 py-2 flex flex-col justify-center ">
+                  {[1, 1, 1, 1, 1].map((list, i) => (
+                    <MessageCard
+                      isReqUserMessage={i%2===0}
+                      content={`this is a sample message from ${i % 2}`}
+                      time={"18:30"}
+                    />
+                  ))}
+                </div>
+              </div>
+              {/* Footer part */}
+              {paymentDone && (
+                <div className="footer absolute bottom-0 py-5 text-lg w-full">
+                  <div className="flex justify-center items-center px-5 relative">
+                    <div className="flex items-center justify-center space-x-4 px-4 absolute left-[16%] text-[#5D5D5D]">
+                      <BsEmojiSmile className="cursor-pointer" />
+                      <ImAttachment className="cursor-pointer" />
+                    </div>
 
-              <div className="h-[85vh]">sample</div>
+                    <input
+                      type="text"
+                      placeholder="Message"
+                      className="py-2 outline-none border-none bg-[#F8F8F9] pl-20 w-[70%] rounded-lg placeholder:text-[#5D5D5D]"
+                      onChange={(e) => {
+                        setContent(e.target.value);
+                      }}
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                          handleCreateMessage();
+                          setContent("");
+                        }
+                      }}
+                      value={content}
+                    />
+                    <IoMdSend
+                      className="text-[#5627B0] absolute left-[80%] cursor-pointer"
+                      onClick={() => {
+                        handleCreateMessage();
+                        setContent("");
+                      }}
+                    />
+                  </div>
+                </div>
+              )}
+
+              {!paymentDone && (
+                <div className="footer absolute bottom-0 py-5 text-lg w-full items-center justify-center flex">
+                  <span className="bg-[#EEE9F7] font-semibold flex justify-center space-x-1 rounded-lg items-center px-5 py-3 w-[80%]">
+                    <p>Looks like you've run out of credits!</p>
+                    <p
+                      className="text-[#5627B0] underline cursor-pointer"
+                      onClick={() => {
+                        setPaymentDone(true);
+                      }}
+                    >
+                      Renew here
+                    </p>
+                  </span>
+                </div>
+              )}
             </div>
           )}
         </div>
