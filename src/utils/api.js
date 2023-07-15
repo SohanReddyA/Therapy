f999023507df91e33ef3fb95841d32b0952732d6
SohanReddyA <pingusohan@gmail.com>
2023-07-16 02:25:22 +0530

    ip

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,3 +1,4 @@
 // export const BASE_API_URL = 'https://friennly.onrender.com';
-export const BASE_API_URL = 'http://localhost:8081';
+// export const BASE_API_URL = 'http://localhost:8081';
+export const BASE_API_URL = 'http://15.206.85.178:8081';
 // export const BASE_API_URL = 'https://frennly.up.railway.app';
