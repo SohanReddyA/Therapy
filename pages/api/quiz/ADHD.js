7ba111f5e3edaa2c22f39d7115c1f61f888f7d5c
SohanReddyA <pingusohan@gmail.com>
2023-09-07 23:40:12 +0530

    Changed domain

--- pages/api/quiz/ADHD.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import data from '../../../lib/ADHD.json';
-export default function handler(req, res) {
-  res.status(200).json(data);
-}
